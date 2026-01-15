2e5df9eaf8fac20ef0f97c09e54aeef207f7b371
Swarnavo2003 <swarnavo2020@gmail.com>
2026-01-15 21:11:07 +0530

    feat(server): swagger api docs added to health check route

--- server/src/routes/health-check.router.js
+++ server/src/routes/health-check.router.js
@@ -3,6 +3,23 @@ import { healthCheck } from "../controllers/health-check.controller.js";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Health Check
+ *   description: Health Check
+ */
+
+/**
+ * @swagger
+ * /api/health-check:
+ *   get:
+ *     summary: Health Check
+ *     tags: [Health Check]
+ *     responses:
+ *       200:
+ *         description: Server is healthy
+ */
 router.get("/", healthCheck);
 
 export default router;
